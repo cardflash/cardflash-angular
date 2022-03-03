c9c45f3d71b6efe8ad8337f20310f56cdc1c7125
aarkue <mail@aarkue.eu>
2022-03-03 20:50:54 +0100

    redirect link clicks to router navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component, OnDestroy} from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnDestroy} from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { DataApiService } from './data-api.service';
 import { AccountService } from './services/account.service';
 import { UserNotifierService } from './services/notifier/user-notifier.service';
@@ -11,18 +13,34 @@ import { UtilsService } from './utils.service';
 })
 export class AppComponent implements AfterViewInit, OnDestroy {
   currentLocale : 'en' | 'de' = 'en';
-  constructor(public accountService: AccountService, public userNotifier : UserNotifierService, public utilService: UtilsService, public dataApi: DataApiService){
+  constructor(public accountService: AccountService, public userNotifier : UserNotifierService, public utilService: UtilsService, public dataApi: DataApiService, private router: Router){
     this.dataApi.fetchConfig()
     this.accountService.updateAcc();
     if(window.location.href.includes('/de/')){
       this.currentLocale = 'de';
     }
   }
+
+
   ngOnDestroy(): void {
   }
 
   ngAfterViewInit(): void {
 
+  }
+
+  @HostListener('window:click', ['$event'])
+  click(event: KeyboardEvent) {
+    const path = event.composedPath() as Array<any>;
+    const firstAnchor : HTMLAnchorElement | null = path.find(p => p && p.tagName && p.tagName.toLowerCase() === 'a');
+    if (firstAnchor && !firstAnchor.hasAttribute('routerlink')) {
+      const href = firstAnchor.getAttribute('href');
+      if(href?.indexOf(environment.BASE_URL) === 0){
+        console.log('internal link pressed',href)
+        this.router.navigateByUrl(href.replace(environment.BASE_URL,''));
+        event.preventDefault();
+      }
+    }
   }
 
 
