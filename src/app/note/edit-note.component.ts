bd20a7724da48f8a57d0e1ac5c42d4ec8785b679
aarkue <mail@aarkue.eu>
2022-04-15 20:53:29 +0200

    added note command prompt (/)

--- src/app/note/edit-note.component.ts
+++ src/app/note/edit-note.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CKEditorComponent } from '@ckeditor/ckeditor5-angular';
 import * as CustomBalloonEditor from 'src/ckeditor/ckeditor.js';
 import { environment } from 'src/environments/environment';
+import { v4 } from 'uuid';
 import { DataApiService, NoteEntry } from '../data-api.service';
 import { UtilsService } from '../utils.service';
 @Component({
@@ -17,91 +18,148 @@ export class EditNoteComponent implements OnInit {
 
   @Input('activeSide') activeSide: string = '';
 
-
   @Input('note') note?: NoteEntry;
   private tempStyles: HTMLStyleElement | undefined;
 
-
   private notes: NoteEntry[] = [];
   public readonly EDITOR_CONFIG = {
-    extraPlugins : [function MentionLinks( editor: any ) {
-      // The upcast converter will convert a view
-      //
-      //		<a href="..." class="mention" data-mention="...">...</a>
-      //
-      // element to the model "mention" text attribute.
-      editor.conversion.for( 'upcast' ).elementToAttribute( {
+    extraPlugins: [
+      function MentionLinks(editor: any) {
+        // The upcast converter will convert a view
+        //
+        //		<a href="..." class="mention" data-mention="...">...</a>
+        //
+        // element to the model "mention" text attribute.
+        editor.conversion.for('upcast').elementToAttribute({
           view: {
-              name: 'a',
-              key: 'data-mention',
-              classes: 'mention',
-              attributes: {
-                  href: true
-              }
+            name: 'a',
+            key: 'data-mention',
+            classes: 'mention',
+            attributes: {
+              href: true,
+            },
           },
           model: {
-              key: 'mention',
-              value: (viewItem : any) => editor.plugins.get( 'Mention' ).toMentionAttribute( viewItem )
+            key: 'mention',
+            value: (viewItem: any) => {console.log({viewItem}); return editor.plugins.get('Mention').toMentionAttribute(viewItem)},
           },
-          converterPriority: 'high'
-      } );
-  
-      // Downcast the model "mention" text attribute to a view
-      //
-      //		<a href="..." class="mention" data-mention="...">...</a>
-      //
-      // element.
-      editor.conversion.for( 'downcast' ).attributeToElement( {
+          converterPriority: 'high',
+        });
+
+        // Downcast the model "mention" text attribute to a view
+        //
+        //		<a href="..." class="mention" data-mention="...">...</a>
+        //
+        // element.
+        editor.conversion.for('downcast').attributeToElement({
           model: 'mention',
-          view: ( modelAttributeValue : any, { writer } : any) => {
-              // Do not convert empty attributes (lack of value means no mention).
-              if ( !modelAttributeValue ) {
-                  return;
+          view: (modelAttributeValue: any, { writer }: any) => {
+            // Do not convert empty attributes (lack of value means no mention).
+            if (!modelAttributeValue) {
+              return;
+              // let item = writer.createAttributeElement('br',{style: 'display: none', 'hidden': true},{priority: 5});
+              // return item;
+            }
+            let href;
+            if(modelAttributeValue.id.indexOf('/') === 0){
+              const command = modelAttributeValue.id;
+              modelAttributeValue.id = '';
+              console.log('exec' + modelAttributeValue.id);
+              let item = writer.createAttributeElement('none',{style: 'display: none', 'hidden': true},{priority: 5});
+              
+              setTimeout(() => {
+                switch (command) {
+                  case '/admonition':
+                    editor.execute('insertAdmonition',{value: 'help'});
+                    editor.execute('delete');
+                    break;
+                    case '/header':
+                      editor.model.change((writer : any) => {
+                        const title = writer.createElement('heading2',{});
+                        // writer.appendText('Title',{},title)
+                        editor.execute( 'heading', { value: 'heading2' } );
+                        return title;
+                        // editor.model.insertContent(title,editor.model.document.selection.getLastPosition(),'after');
+                      })
+                      break;
+                    case '/diagram':
+                      editor.execute("insertDiagram");
+                      editor.execute("editDiagram");
+                      break;
+                      default:
+                        break;
+                      }
+                    },10);
+              return item;
+            }else if(modelAttributeValue.id.indexOf('[') === 0){
+              if (modelAttributeValue.href) {
+                href = modelAttributeValue.href;
+              } else {
+                href =
+                  environment.BASE_URL +
+                  '/notes/' +
+                  modelAttributeValue.id.replace('[[', '').replace(']]', '');
               }
   
-              let href;
-  
-              
-                console.log({modelAttributeValue});
-                if(modelAttributeValue.href){
-                  href = modelAttributeValue.href;
-                }else{
-                  href = environment.BASE_URL + '/notes/' + modelAttributeValue.id.replace('[[','').replace(']]','');
-                }
-              
-  
-              return writer.createAttributeElement( 'a', {
+              return writer.createAttributeElement(
+                'a',
+                {
                   class: 'mention',
                   'data-mention': modelAttributeValue.id,
-                  href
-              }, {
+                  href,
+                },
+                {
                   // Make mention attribute to be wrapped by other attribute elements.
                   priority: 20,
                   // Prevent merging mentions together.
-                  id: modelAttributeValue.uid
-              } );
+                  id: modelAttributeValue.uid,
+                }
+              );
+
+            }else{
+              let item = writer.createAttributeElement('none',{style: 'display: none', 'hidden': true},{priority: 5});
+              // setTimeout(() => {
+              //   editor.execute('delete');
+              // },1)
+              return item;
+              // return null;
+            }
           },
-          converterPriority: 'high'
-      } );
-  }],
+          converterPriority: 'high',
+        });
+      },
+    ],
     mention: {
       feeds: [
-    {marker: '[',
-    feed: this.getSuggestions.bind(this),
-   minimumCharacters: 1,
-   itemRenderer: this.customItemRenderer.bind(this)}
-      ]}
-  }
-  constructor(public utils: UtilsService, private dataApi: DataApiService, private actRoute: ActivatedRoute) {}
+        {
+          marker: '[',
+          feed: this.getSuggestions.bind(this),
+          minimumCharacters: 1,
+          itemRenderer: this.customItemRenderer.bind(this),
+        },
+        {
+          marker: '/',
+          feed: this.getCommandSuggestions.bind(this),
+          minimumCharacters: 0,
+          itemRenderer: this.customCommandRenderer.bind(this)
+        }
+      ],
+    },
+  };
+  constructor(
+    public utils: UtilsService,
+    private dataApi: DataApiService,
+    private actRoute: ActivatedRoute
+  ) {}
 
   async ngOnInit() {
     this.Editor = CustomBalloonEditor;
-    if(!this.note){
+    if (!this.note) {
       this.note = await this.dataApi.getNote(this.actRoute.snapshot.params.id);
     }
     this.dataApi.listNotes(true).then((notes) => {
       this.notes = notes;
-    })
+    });
     // setInterval(() => {
     //   this.addTempStyles();
     // }, 500);
@@ -185,7 +243,7 @@ export class EditNoteComponent implements OnInit {
         console.log(el);
         return el.name === 'admonition';
       });
-      if(admonitions.length === 0){
+      if (admonitions.length === 0) {
         this.newCard();
         this.addAnnotation(annotation);
         return;
@@ -203,7 +261,9 @@ export class EditNoteComponent implements OnInit {
             this.editorComponent?.editorInstance.model.insertContent(
               modelFragment,
               writer.createPositionAt(
-                this.activeSide === 'front' ? admonition_title.getChild(0) : admonition_content.getChild(0),
+                this.activeSide === 'front'
+                  ? admonition_title.getChild(0)
+                  : admonition_content.getChild(0),
                 'end'
               )
             );
@@ -213,14 +273,14 @@ export class EditNoteComponent implements OnInit {
     }
   }
 
-  newCard(){
+  newCard() {
     if (this.editorComponent && this.editorComponent.editorInstance) {
       console.log(this.editorComponent);
       // this.editorComponent.editorInstance.model.change((writer: any) => {
       //   writer.insertElement('admonition',[],document.)
       // })
       // this.editorComponent.editorInstance.g
-      this.editorComponent.editorInstance.execute('insertAdmonition',{value: 'help'})
+      this.editorComponent.editorInstance.execute('insertAdmonition', { value: 'help' });
       // this.editorComponent.editorInstance.execute('enter',)
       this.editorComponent?.editorInstance.model.change((writer: any) => {
         if (this.editorComponent && this.editorComponent.editorInstance) {
@@ -236,64 +296,152 @@ export class EditNoteComponent implements OnInit {
     }
   }
 
-  async save(){
-    if(this.note){
-      console.log(this.note)
-      await this.dataApi.updateNote(this.note.$id,this.note)
+  async save() {
+    if (this.note) {
+      await this.dataApi.updateNote(this.note.$id, this.note);
     }
   }
-  async change(){
-   this.save();
+  async change() {
+    this.save();
   }
 
-  async getSuggestions(queryText: string){
-    const searchStrings = queryText.toLowerCase().replace('[','').replace(']','').split('#');
+  async getSuggestions(queryText: string) {
+    const searchStrings = queryText.toLowerCase().replace('[', '').replace(']', '').split('#');
     const docSearchStrings = searchStrings.length === 0 ? [] : searchStrings[0].split(' ');
     // const res = await this.dataApi.listDocuments(true);
     // console.log({res})
-    const relevant_docs =  this.notes.filter((doc) => {
+    const relevant_docs = this.notes.filter((doc) => {
       for (let i = 0; i < docSearchStrings.length; i++) {
-        if(!doc.title.toLowerCase().includes(docSearchStrings[i])){
+        if (!doc.title.toLowerCase().includes(docSearchStrings[i])) {
           return false;
         }
       }
       return true;
     });
-    const suggestions = queryText.includes('#') ? [] : relevant_docs.map((doc) => {
-      return {id: `[[${doc.$id}]]`, href: environment.BASE_URL+'/notes/'+doc.$id, text: `[[${doc.title}]]`, docName: doc.title, cardContent: '', isNew: false}
-    })
-    if(queryText.length >= 2 && !queryText.includes(']')){
-      suggestions.push({id: `[[${queryText.replace('[','')}]]`, text: `[[${queryText.replace('[','')}]]`, docName: queryText.replace('[',''), cardContent: '', isNew: true, href: environment.BASE_URL+'/notes'})
+    const suggestions = queryText.includes('#')
+      ? []
+      : relevant_docs.map((doc) => {
+          return {
+            id: `[[${doc.$id}]]`,
+            href: environment.BASE_URL + '/notes/' + doc.$id,
+            text: `[[${doc.title}]]`,
+            docName: doc.title,
+            cardContent: '',
+            isNew: false,
+          };
+        });
+    if (queryText.length >= 2 && !queryText.includes(']')) {
+      suggestions.push({
+        id: `[[${queryText.replace('[', '')}]]`,
+        text: `[[${queryText.replace('[', '')}]]`,
+        docName: queryText.replace('[', ''),
+        cardContent: '',
+        isNew: true,
+        href: environment.BASE_URL + '/notes',
+      });
     }
     // let promises : Promise<void>[] = [];
     // await Promise.all(promises);
-    console.log({suggestions})
+    console.log({ suggestions });
     return suggestions;
-  
   }
 
-  customItemRenderer(item: {id: string, href: string, text: string, docName: string, cardContent: string, isNew: boolean}){
-    const itemElement = document.createElement( 'span' );
+  customItemRenderer(item: {
+    id: string;
+    href: string;
+    text: string;
+    docName: string;
+    cardContent: string;
+    isNew: boolean;
+  }) {
+    const itemElement = document.createElement('span');
 
-    itemElement.classList.add( 'custom-item' );
-    itemElement.id = `mention-list-item-id-${ item.id }`;
+    itemElement.classList.add('custom-item');
+    itemElement.id = `mention-list-item-id-${item.id}`;
 
-    const docNameShort = (item.docName.length > 20) ? (item.docName.substring(0,18) + '...') : (item.docName);
+    const docNameShort =
+      item.docName.length > 20 ? item.docName.substring(0, 18) + '...' : item.docName;
     itemElement.textContent = docNameShort;
-    if(item.isNew){
+    if (item.isNew) {
       itemElement.textContent += ' (create new)';
     }
     itemElement.style.fontWeight = 'bold';
     itemElement.style.display = 'inline-block';
-    
-    if(item.cardContent){
-      const cardElement = document.createElement( 'span' );
-      cardElement.classList.add( 'custom-item-username' );
-      const cardContentShort = (item.cardContent.length > 35) ? (item.cardContent.substring(0,33) + '...') : (item.cardContent);
+
+    if (item.cardContent) {
+      const cardElement = document.createElement('span');
+      cardElement.classList.add('custom-item-username');
+      const cardContentShort =
+        item.cardContent.length > 35 ? item.cardContent.substring(0, 33) + '...' : item.cardContent;
       cardElement.textContent = ': 🗃️ ' + cardContentShort;
-      itemElement.appendChild( cardElement );
+      itemElement.appendChild(cardElement);
     }
 
+    return itemElement;
+  }
+
+  async getCommandSuggestions(queryText: string) {
+    if(queryText.includes(' ')){
+      return [];
+    }
+    console.log({queryText});
+    const allCommands = [{
+      id: `/diagram`,
+      commandName: `Insert diagram`,
+      text: ' ',
+      type: 'command',
+      icon: 'mediation'
+    },
+    {
+      id: `/header`,
+      commandName: `Insert header`,
+      text: ' ',
+      type: 'command',
+      icon: 'title'
+    },
+    {
+      id: `/admonition`,
+      commandName: `Insert admonition`,
+      text: ' ',
+      type: 'command',
+      icon: 'info'
+    }];
+    return allCommands.filter((command) => command.commandName.includes(queryText));
+  }
+  
+
+  customCommandRenderer(item: {
+    id: string;
+    text: string;
+    commandName: string;
+    icon: string;
+  }) {
+    // <i class="material-icons">image</i>
+    const iconEl = document.createElement('i');
+    iconEl.classList.add('material-icons')
+    iconEl.textContent = item.icon;
+    iconEl.style.fontFamily = '"Material Icons"';
+    iconEl.style.fontSize = '1.5rem';
+
+    const itemElement = document.createElement('span');
+    itemElement.style.display = 'inline-flex';
+    itemElement.style.justifyContent = 'space-between';
+    itemElement.style.alignItems = 'center';
+    itemElement.style.minWidth = '10rem';
+    itemElement.id = `mention-list-item-id-${item.id}`;
+    itemElement.classList.add('command-item-element');
+    const itemLabel = document.createElement('span');
+    
+    // itemElement.classList.add('command-item');
+    
+    // itemLabel.style.fontWeight = 'bold';
+    itemLabel.style.fontSize = '1.1rem';
+    itemLabel.style.width = '100%';
+    // itemLabel.style.textAlign = 'center';
+    itemLabel.style.marginLeft = '1rem';
+    itemLabel.textContent = item.commandName;
+    itemElement.appendChild(iconEl);
+    itemElement.appendChild(itemLabel);
     return itemElement;
   }
 }
